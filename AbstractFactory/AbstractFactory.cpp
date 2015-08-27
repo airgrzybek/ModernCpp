6c96428ab6f4f08e38fa7e027fd634268f23943d
unknown <xxpowdaw@BROPC45QJZ42.eu.brightone.eu>
2015-08-27 13:02:18 +0200

    AbstractFactory initial commit

---
+++ AbstractFactory/AbstractFactory.cpp
@@ -0,0 +1,20 @@
+/*
+ * AbstractFactory.cpp
+ *
+ *  Created on: 27 sie 2015
+ *      Author: xxpowdaw
+ */
+
+#include "AbstractFactory.h"
+
+AbstractFactory::AbstractFactory()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+AbstractFactory::~AbstractFactory()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ AbstractFactory/AbstractFactory.h
@@ -0,0 +1,18 @@
+/*
+ * AbstractFactory.h
+ *
+ *  Created on: 27 sie 2015
+ *      Author: xxpowdaw
+ */
+
+#ifndef ABSTRACTFACTORY_ABSTRACTFACTORY_H_
+#define ABSTRACTFACTORY_ABSTRACTFACTORY_H_
+
+class AbstractFactory
+{
+public:
+    AbstractFactory();
+    virtual ~AbstractFactory();
+};
+
+#endif /* ABSTRACTFACTORY_ABSTRACTFACTORY_H_ */
