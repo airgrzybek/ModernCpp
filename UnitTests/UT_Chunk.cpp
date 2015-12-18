55c3cf85246326cf60af5193d663e5e9699e46f6
unknown <xxpowdaw@BROPC45QJZ42.eu.brightone.eu>
2015-12-18 13:53:40 +0100

    UT for Chunk

--- UnitTests/UT_Chunk.cpp
+++ UnitTests/UT_Chunk.cpp
@@ -5,53 +5,15 @@
 
 const static unsigned char MAX_U8 = 255;
 
-class ChunkTest : public testing::Test
+class ChunkTestParam : public testing::TestWithParam<std::size_t>
 {
 public:
-    ChunkTest()
-    {
-
-    }
-    ~ChunkTest()
-    {
-
-    }
-
-    void SetUp()
-    {
-    }
-
-    void TearDown()
-    {
-        chunk.Release();
-    }
-
-    Memory::Chunk chunk;
-};
-
-TEST_F(ChunkTest, Init)
-{
-    chunk.Init(4,255);
-
-    unsigned char * data = chunk.pData;
-    EXPECT_TRUE(data);
-
-
-    for(unsigned char i = 1; i < MAX_U8; ++i, data +=4)
-    {
-        EXPECT_EQ(i,*data);
-    }
-}
-
-class ChunkTestPAram : public testing::TestWithParam<std::size_t>
-{
-public:
-    ChunkTestPAram() :
+    ChunkTestParam() :
         blockSize(0)
     {
 
     }
-    ~ChunkTestPAram()
+    ~ChunkTestParam()
     {
 
     }
@@ -70,7 +32,7 @@ public:
     Memory::Chunk chunk;
 };
 
-TEST_P(ChunkTestPAram, Init)
+TEST_P(ChunkTestParam, Init)
 {
     unsigned char * data = chunk.pData;
     EXPECT_TRUE(data);
@@ -81,11 +43,7 @@ TEST_P(ChunkTestPAram, Init)
     }
 }
 
-INSTANTIATE_TEST_CASE_P(InstantiationName,
-                        ChunkTestPAram,
-                        ::testing::Values(1,2,4,9,48));
-
-TEST_P(ChunkTestPAram, Allocate)
+TEST_P(ChunkTestParam, Allocate)
 {
     void * p = chunk.Allocate(blockSize);
     EXPECT_TRUE(p);
@@ -93,14 +51,14 @@ TEST_P(ChunkTestPAram, Allocate)
     EXPECT_EQ(1,chunk.firstAvailableBlock);
 }
 
-TEST_P(ChunkTestPAram, Allocate_NULL)
+TEST_P(ChunkTestParam, Allocate_NULL)
 {
     chunk.blocksAvailable=0;
     void * p = chunk.Allocate(blockSize);
     EXPECT_FALSE(p);
 }
 
-TEST_P(ChunkTestPAram, Deallocate)
+TEST_P(ChunkTestParam, Deallocate)
 {
     void * p = chunk.Allocate(blockSize);
     EXPECT_TRUE(p);
@@ -111,3 +69,45 @@ TEST_P(ChunkTestPAram, Deallocate)
     EXPECT_EQ(MAX_U8,chunk.blocksAvailable);
     EXPECT_EQ(0,chunk.firstAvailableBlock);
 }
+
+TEST_P(ChunkTestParam, Deallocate_Death)
+{
+    void * p = 0;
+    ASSERT_DEATH(chunk.Deallocate(p,blockSize),"Assertion failed: .*Chunk.cpp");
+}
+
+TEST_P(ChunkTestParam, HasBlock)
+{
+    void * p = chunk.Allocate(blockSize);
+
+    EXPECT_TRUE(chunk.HasBlock(p,blockSize));
+}
+
+TEST_P(ChunkTestParam,overflow)
+{
+    for(unsigned char i = 0; i < MAX_U8; ++i)
+    {
+        unsigned char * p = static_cast<unsigned char *>(chunk.Allocate(blockSize));
+        *p = 0;
+    }
+    EXPECT_FALSE(chunk.Allocate(blockSize));
+    EXPECT_EQ(0, chunk.blocksAvailable);
+    EXPECT_EQ(255, chunk.firstAvailableBlock);
+}
+
+TEST_P(ChunkTestParam, Release)
+{
+    chunk.Release();
+
+    EXPECT_FALSE(chunk.pData);
+    EXPECT_EQ(0,chunk.blocksAvailable);
+}
+
+TEST_P(ChunkTestParam, HasAvailible)
+{
+    EXPECT_TRUE(chunk.HasAvailable(MAX_U8));
+}
+
+INSTANTIATE_TEST_CASE_P(InstantiationName,
+                        ChunkTestParam,
+                        ::testing::Values(1,2,4,9,48));
